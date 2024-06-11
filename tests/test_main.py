100abeddf05aa047bcfb3f25dfe0f4d0b629f545
H4ckeurM4n <camille.he@gmx.com>
2024-06-11 12:12:23 +0200

    Ajout du setup, requierement, et code du test

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,10 @@ from src.main import add
 class TestMain(unittest.TestCase):
 
     def test_add(self):
+        self.assertEqual(add(2, 4), 6)
+        self.assertEqual(add(-1, 3), 2)
+        self.assertEqual(add(-1, -2), -3)
+        
         pass
 
 if __name__ == '__main__':
